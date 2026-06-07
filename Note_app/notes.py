ad5a63f11862820dce25f2bd56b53a8092bac7ae
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-06-07 05:58:03 +0530

    Added user details route

--- Note_app/notes.py
+++ Note_app/notes.py
@@ -14,6 +14,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def redirect_home_page():
     return RedirectResponse("/home")
+
+# Always return current date
 today = date.today()
 
 @app.get("/home", response_class=HTMLResponse)
@@ -21,39 +23,50 @@ def home_page(request:Request):
     cur.execute("SELECT *FROM  User_record")
     notes = cur.fetchall()
     return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={"message":"Welcome to My Note App","notes":notes}
+        request=request,name="index.html",
+        context={"message":"Welcome to My Note App","notes":notes})
+
+# getting name of user 
+@app.post("/details/{note_id}", response_class=HTMLResponse)
+def user_detail(request:Request, note_id:int):
+   cur.execute("Select *from User_record where id=%s", (note_id))
+   note = cur.fetchone()
+   if not note:
+       return RedirectResponse("/home", status_code=303) 
+   else:
+        return templates.TemplateResponse(
+        request=request,name="detail.html",
+        context={"message":"Your Note added Successfully!"}
     )
        
 @app.post("/add", response_class=HTMLResponse)
-def add_notes(request:Request,Work_type:str=Form(),Content:str=Form(), creation:str=Form()):
+def add_notes(request:Request,Work_type:str=Form(),Content:str=Form(), Creation_date:str=Form()):
     Work_type=Work_type.title()
     creation = str(date.today())
     print(Work_type)
     print(Content)
-    print(creation)
+    print(Creation_date)
     query = """
-    Insert into User_record(Work_type, Content, creation)
+    Insert into User_record(Work_type, Content, creation_date)
     values(%s,%s,%s)
     """
-    values = (Work_type,Content,creation)
+    values = (Work_type,Content,Creation_date)
     cur.execute(query, values)
     conn.commit()
     cur.execute("Select *from User_record") 
     notes = cur.fetchall()
 
     return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={"message":"Note added Successfully!", "notes":notes}
-    )
+        request=request,name="index.html",
+        context={"message":"Note added Successfully!", "notes":notes})
+
+
 @app.post("/delete/{note_id}")
 def delete_user(note_id:int):
     query = "DELETE FROM User_record where id=%s"
     values = (note_id,)
     cur.execute(query, values)
-    conn.commit()
     return RedirectResponse("/home", status_code=303)
+    conn.commit()
     cur.close()
 
