69a914a8632db1f376e23b01809ed9dcd345023d
adityaupadhayay066-droid <adityaupadhayay066@gmail.com>
2026-06-14 14:57:39 +0530

    Adding schema of note app

---
+++ Note_app/Noteapp schema.sql
@@ -0,0 +1,15 @@
+use Notes;
+show Databases;
+Create table User_record(
+id int  auto_increment primary key,
+Work_type varchar(50),
+Content Varchar(200),
+Creation Date
+);
+select *from User_record;
+drop table User_record;
+TRUNCATE User_record;
+DESC User_Record;
+Alter table User_record
+change  Creation  Creation_date DATE;
+drop database ai_tutor;
