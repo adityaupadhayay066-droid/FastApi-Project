771354f32eb152eeb2c0783775cf15a3ccf99f69
Aditya Upadhayay <adityaupadhayay066@gmail.com>
2026-06-28 20:46:51 +0530

    Delete Age_Calculator directory

--- Age_Calculator/logic.py
+++
@@ -1,8 +0,0 @@
-from datetime import date,datetime
-
-def calculate_age(day:int, month:int,year:int):
-    today=date.today()
-    age=today.year-year
-    if(today.month,today.day)<(month,day):
-        age-=1;
-        return age;
